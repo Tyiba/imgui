ca0cfdff3a6844940a60d5ef975b15a35e6552dd
Tyiba <91595082+Tyiba@users.noreply.github.com>
2025-03-24 23:24:35 +0000

    added precompiled headers for compatibility

--- imgui.cpp
+++ imgui.cpp
@@ -1058,6 +1058,7 @@ CODE
 // [SECTION] INCLUDES
 //-------------------------------------------------------------------------
 
+#include "pch.h"
 #if defined(_MSC_VER) && !defined(_CRT_SECURE_NO_WARNINGS)
 #define _CRT_SECURE_NO_WARNINGS
 #endif
--- imgui_widgets.cpp
+++ imgui_widgets.cpp
@@ -32,6 +32,7 @@ Index of this file:
 
 */
 
+#include "pch.h"
 #if defined(_MSC_VER) && !defined(_CRT_SECURE_NO_WARNINGS)
 #define _CRT_SECURE_NO_WARNINGS
 #endif
